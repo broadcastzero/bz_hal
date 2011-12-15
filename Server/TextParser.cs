e68121eebcad353617610f2c41de5c36b7f0ffde
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-15 13:09:47 +0100

    TextParser delegates calculating type of word to extra Word class. Words are saved in a list, word position = list position.

--- Server/TextParser.cs
+++ Server/TextParser.cs
@@ -3,21 +3,28 @@
 /* FUNCTION: Split sentence into words, analyse them and save their position in the sentence */
 
 using System;
+using System.Collections.Generic;
 
 namespace Server
 {
     public class TextParser
     {
         private string _clientsentence;
-        private char[][] _analysedword;
+        private List<Word> _Analysedword;  
 
         public TextParser(string cs)
-        { 
+        {
             _clientsentence = cs;
+
+            //split sentence into words and save it into list
+            //for...
+            Word nword = new Word();
+            _Analysedword.Add(nword);
         }
 
         public void SplitSentence()
         {
+
             //save type info (N...noun, V...Verb, A...adjective, R...article, P...preposition)
         }
     }
---
+++ Server/Word.cs
@@ -0,0 +1,14 @@
+﻿/* NS: Server */
+/* FN: Word.cs */
+/* FUNCTION: Single word of sentence, calculate type */
+
+using System;
+
+namespace Server
+{
+    public class Word
+    {
+        private char _type;
+        public char type { get; set; }
+    }
+}
