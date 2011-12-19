b8dcd75c5a20b78d6af36e8a4895817800167b76
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-19 15:44:05 +0100

    Unit test added - tests, if words are saved correctly in List<Word>

--- Server/TextParser.cs
+++ Server/TextParser.cs
@@ -12,25 +12,27 @@ namespace Server
     public class TextParser
     {
         /* PRIVATE VARS */
+        private string _ClientSentence;
         /* PUBLIC VARS */
-        public string ClientSentence { get; set; }
-        public List<Word> AnalysedWord { get; set; }
+        public List<Word> AnalysedWords { get; set; }
 
         public void SplitSentence(string cs)
         {
             //pass and split sentence
-            ClientSentence = cs;
+            _ClientSentence = cs;
             char[] seps = { ' ', '.' };
             string[] words = cs.Split(seps);
 
             //create word-instance for each word and save it into list
-            AnalysedWord = new List<Word>();
+            AnalysedWords = new List<Word>();
             foreach(string w in words)
             {
                 Word nword = new Word(w);
                 nword.Type = this.GetType(nword);
-                AnalysedWord.Add(nword);
+                AnalysedWords.Add(nword);
             }
+            //remove newline
+            AnalysedWords.RemoveAt(AnalysedWords.Count-1);
         }
 
         private char GetType(Word w)
---
+++ TextParserTest/SplitsCorrectlyTest.cs
@@ -0,0 +1,40 @@
+﻿/* NS: TextParserTest */
+/* FN: SplitsCorrectlyTest.cs */
+/* FUNCTION: Test the text parser, if words are split correctly and if they are saved in list */
+
+using System;
+using NUnit.Framework;
+using Server;
+
+namespace TextParserTest
+{
+    [TestFixture]
+    public class SplitsCorrectlyTest
+    {
+        private TextParser _Tp = null;
+
+        /* Initialise */
+        [SetUp]
+        public void SetUp()
+        {
+            _Tp = new TextParser();
+        }
+
+        /* Test if words are split correctly */
+        [Test]
+        public void SplitTest()
+        {
+            _Tp = new TextParser();
+            string s = "Das ist mein Testsatz.";
+            _Tp.SplitSentence(s);
+            Assert.IsNotEmpty(_Tp.AnalysedWords);
+            Assert.That(_Tp.AnalysedWords[0].Value == "Das");
+            Assert.That(_Tp.AnalysedWords[1].Value == "ist");
+            Assert.That(_Tp.AnalysedWords[2].Value == "mein");
+            Assert.That(_Tp.AnalysedWords[3].Value == "Testsatz");
+            foreach (Word w in _Tp.AnalysedWords)
+            { Console.WriteLine(w.Value); }
+            Console.WriteLine("--End of sentence--");
+        }
+    }
+}
