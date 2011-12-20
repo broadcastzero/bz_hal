ff59822c5c671e0630d7ba6d7056df9d3e808d78
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-20 10:39:06 +0100

    Wrapper for PluginManager methods written

--- Server/PluginManager.cs
+++ Server/PluginManager.cs
@@ -1,15 +1,27 @@
 ﻿/* NS: Server */
 /* FN: PluginManager.cs */
-/* FUNCTION: Receive analysed sentence from TextParser, scan existing plugins and send sentence to them. */
+/* FUNCTION: Receive analysed wordlist from ClientComm, scan existing plugins and send wordlist to them. */
 /*              Receive priority answer from plugins and choose the one with the highest priority. */
-/*              Pass answer to SendAnswerToClient-class */
+/*              Return answer to ClientComm-class */
 
 using System;
+using System.Collections.Generic;
 
 namespace Server
 {
     public class PluginManager
     {
+        /* CONSTRUCTOR - precache plugings, throw exception if no plugins could be loaded */
+        public PluginManager()
+        { 
 
+        }
+
+        /* Send wordlist to plugins and return answerstring to ClientComm */
+        public string SendListToPlugins(List<Word> wlist)
+        {
+            string answer = "Hier koennte Ihre Antwort stehen.";
+            return answer;
+        }
     }
 }
