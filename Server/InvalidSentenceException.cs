d31fc9f2faef15e4c0937e62c836308a4454de0c
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-20 09:42:38 +0100

    Exception written, which will be raised if sentence doesnt end with a . or a ?

---
+++ Server/InvalidSentenceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Server
+{
+    public class InvalidSentenceException : Exception
+    {
+        public InvalidSentenceException(string message) : base (message){ }
+
+        public InvalidSentenceException(string message, Exception inner)
+            : base(message, inner)
+        {   }
+    }
+}
