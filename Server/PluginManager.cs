a2da96fd6f9969838d3af3678edb70cd99f3b163
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-21 10:24:40 +0100

    Fixes problem with Environment.CurrentDirectory when Nunit testing

--- Server/PluginManager.cs
+++ Server/PluginManager.cs
@@ -20,7 +20,9 @@ namespace Server
         /* CONSTRUCTOR - precache plugings, throw exception if no plugins could be loaded */
         public PluginManager()
         {
-            _PlugPath = Environment.CurrentDirectory += "\\Plugins\\";
+            //work now with absolute path (because Nunit test would throw exception when using CurrentDir
+            //maybe change to Environment.CurrentDirectory later!
+            _PlugPath = "C:\\Users\\broadcastzero\\0 FH\\3. Semester\\GPR3\\bz_hal\\Server\\bin\\Debug\\Plugins\\";
             if (!Directory.Exists(_PlugPath))
             {
                 throw new FileNotFoundException("Das Plugin-Verzeichnis konnte nicht geoeffnet werden!");
--- TestsNunit/PluginManagerTest/ReadPluginsTest.cs
+++ TestsNunit/PluginManagerTest/ReadPluginsTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using Server;
+using System.IO;
 
 namespace PluginManagerTest
 {
@@ -9,12 +10,29 @@ namespace PluginManagerTest
     public class ReadPluginsTest
     {
         private PluginManager _Pm = null;
+        private string _PluginPath = null;
 
         [Test]
         public void ReadPluginFolderTest()
         { 
+            //set PluginPath to server-folder
+            string path="";
+            _PluginPath = Environment.CurrentDirectory;
+            string[] parts = _PluginPath.Split('\\');
+            int i = 0;
+            do
+            {
+                path += parts[i];
+                path += "\\";
+                i++;
+            } while (parts[i] != "bz_hal");
+            path += "bz_hal\\Server\\bin\\Debug\\Plugins\\";
+            Console.WriteLine(path);
+            Console.WriteLine(_PluginPath);
+
             _Pm = new PluginManager();
             Assert.That(PluginManager.PluginList != null);
+           Assert.That(Directory.Exists(path));
         }
     }
 }
