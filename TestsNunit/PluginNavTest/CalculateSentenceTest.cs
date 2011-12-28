310f7593b96a994d861d94cd9eff0d5a1d4cf7f1
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-28 15:02:19 +0100

    Adds wrapper for navigator tests

---
+++ TestsNunit/PluginNavTest/CalculateSentenceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PluginNav;
+using Interface;
+
+namespace PluginNavTest
+{
+    [TestFixture]
+    public class CalculateSentenceTest
+    {
+        private PluginNavi navi = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            navi = new PluginNavi();
+        }
+
+        /* Test if loading the xml-file works */
+        [Test]
+        public void LoadListTest()
+        { 
+            
+        }
+
+        /* Test if searching works */
+        [Test]
+        public void SearchTest()
+        {
+            Assert.That(1 == 3); // add senseful code here
+        }
+
+    }
+}
