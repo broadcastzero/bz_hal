b514ac3952e95a19b974ea92060b3a24bc13a7da
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-28 14:19:37 +0100

    Adds another two tests for PluginNav and sets priority to 10(highest) if all factors fit

--- PluginNav/PluginNavi.cs
+++ PluginNav/PluginNavi.cs
@@ -12,12 +12,13 @@ namespace PluginNav
             int priority = 0;
             // defines, whether string "wo" or streetname have been found before - only count once!
             bool wo_counted = false;
-            bool str_counted = false;            
+            bool str_counted = false;
+            bool isquestion = false;
 
             foreach(Word w in wordlist)
             {
                 if (w.Type == 'Q' && w.Value.ToLower() == "wo" && wo_counted == false)
-                { priority += 5; wo_counted = true; }
+                { priority += 5; wo_counted = true; isquestion = true; }
                 else if((w.Value.ToLower().Contains("strasse") || w.Value.ToLower().Contains("straße") || 
                     w.Value.ToLower().Contains("dorf")) && str_counted == false)
                 { priority += 2; str_counted = true; }
@@ -25,6 +26,10 @@ namespace PluginNav
                 else if (w.Type == 'M' && w.Value == "?")
                 { priority += 1; }
             }
+
+            // if everything fits -> 10 (highest)
+            if (wo_counted == true && str_counted == true && isquestion == true)
+            { priority = 10; }
 
             return priority;
         }
---
+++ TestsNunit/PluginNavTest/GetPriorityTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PluginNav;
+using Interface;
+
+namespace PluginNavTest
+{
+    [TestFixture]
+    public class GetPriorityTest
+    {
+        public List<Word> wordlist = null;
+        public PluginNavi navi = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.wordlist = new List<Word>();
+            navi = new PluginNavi();
+        }
+
+        /* Check highest priority */
+        [Test]
+        public void GetAPriorityTest()
+        {
+            Word w1 = new Word("Wo");
+            w1.Type = 'Q';
+            wordlist.Add(w1);
+            Word w2 = new Word("ist");
+            w2.Type = 'V';
+            wordlist.Add(w2);
+            Word w3 = new Word("die");
+            w3.Type = 'R';
+            wordlist.Add(w3);
+            Word w4 = new Word("Alserstrasse");
+            w4.Type = 'N';
+            wordlist.Add(w4);
+            Word w5 = new Word("?");
+            w5.Type = 'M';
+            wordlist.Add(w5);
+
+            int prior = navi.GetPriority(wordlist);
+            Assert.AreEqual(10, prior);
+        }
+
+        /* Check medium priority */
+        [Test]
+        public void GetAnotherPriorityTest()
+        {
+            Word w1 = new Word("Ich");
+            w1.Type = 'S';
+            wordlist.Add(w1);
+            Word w2 = new Word("habe");
+            w2.Type = 'V';
+            wordlist.Add(w2);
+            Word w3 = new Word("eine");
+            w3.Type = 'X';
+            wordlist.Add(w3);
+            Word w4 = new Word("Straße");
+            w4.Type = 'N';
+            wordlist.Add(w4);
+            Word w5 = new Word("!");
+            w5.Type = 'M';
+            wordlist.Add(w5);
+
+            int prior = navi.GetPriority(wordlist);
+            Assert.AreEqual(2, prior);
+        }
+
+        /* Check if "Straßen" is not counted 2 times */
+        [Test]
+        public void GetLastPriorityTest()
+        {
+            Word w1 = new Word("Straßen");
+            w1.Type = 'S';
+            wordlist.Add(w1);
+            Word w2 = new Word("sind");
+            w2.Type = 'V';
+            wordlist.Add(w2);
+            Word w3 = new Word("Straßen");
+            w3.Type = 'N';
+            wordlist.Add(w3);
+            Word w4 = new Word("Kind");
+            w4.Type = 'N';
+            wordlist.Add(w4);
+            Word w5 = new Word("!");
+            w5.Type = 'M';
+            wordlist.Add(w5);
+
+            int prior = navi.GetPriority(wordlist);
+            Assert.AreEqual(2, prior);
+        }
+    }
+}
