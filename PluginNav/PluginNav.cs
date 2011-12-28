39cb4dc07b3305e596dab63e372af3ab479ee289
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-28 13:14:01 +0100

    Priority function for navigator written.

--- PluginNav/PluginNav.cs
+++ PluginNav/PluginNav.cs
@@ -6,9 +6,24 @@ namespace PluginNav
 {
     public class PluginNav : IPlugin
     {
+        /* Calculate priority of Navigator */
         public int GetPriority(List<Word> wordlist)
         {
-            return 0;
+            int priority = 0;
+            // defines, whether string "wo" has been found before
+            bool wo_counted = false;
+            bool str_counted = false;
+
+            foreach(Word w in wordlist)
+            {
+                if (w.Value.ToLower() == "wo" && wo_counted == false)
+                { priority += 5; wo_counted = true; }
+                else if((w.Value.ToLower() == "strasse" || w.Value.ToLower() == "straße" || 
+                    w.Value.ToLower() == "dorf") && str_counted == false)
+                { priority += 2; str_counted = true; }
+            }
+
+            return priority;
         }
 
         public string CalculateSentence(List<Word> wordlist)
