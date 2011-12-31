3e1e9c12dfa229fac1e86472cf1203c61a6eb602
broadcastzero <broadcastzero@kaffeeschluerfer.com>
2011-12-31 12:47:51 +0100

    Adds basic responsibility calculating function for psychiater

--- PluginPsych/PluginPsych.cs
+++ PluginPsych/PluginPsych.cs
@@ -9,10 +9,40 @@ namespace PluginPsych
         private string _Name = "Psychater";
         public string Name { get { return _Name; } }
 
+        private List<string> _Responsibles = null;
+
+        /* CONSTRUCTOR - add responsibles */
+        public PluginPsych()
+        {
+            _Responsibles = new List<string>();
+            _Responsibles.Add("Vater");
+            _Responsibles.Add("Mutter");
+            _Responsibles.Add("Kinder");
+            _Responsibles.Add("Familie");
+            _Responsibles.Add("Haustier");
+            _Responsibles.Add("Hund");
+            _Responsibles.Add("Katze");
+        }
+
+
         public int GetPriority(List<Word> wordlist)
         {
-
-            return 1;
+            int prior = 0;
+            foreach (Word w in wordlist)
+            {
+                switch (w.Type)
+                { 
+                    case 'N':
+                        if (_Responsibles.Contains(w.Value))
+                        {
+                            prior += 3;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return prior;
         }
 
         public string CalculateSentence(List<Word> wordlist)
